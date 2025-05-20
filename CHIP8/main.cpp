cf2ad4823e1c52be93c70f905946c5128edb2d2b
PastVision <anuragtrivedi96@live.com>
2025-05-20 20:05:14 +0530

    Migrate to SDL2; Emulate in background x10

--- CHIP8/main.cpp
+++ CHIP8/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <SDL3/SDL.h>
+#include "SDL.h"
 #include "chip8.h"
 
 
@@ -12,12 +12,12 @@ bool initSDL(SDL_Window*& window, SDL_Renderer*& renderer) {
 		std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
 		return false;
 	}
-	window = SDL_CreateWindow("CHIP8 Emulator v1", DISPLAY_WIDTH * SCALE, DISPLAY_HEIGHT * SCALE, SDL_WINDOW_VULKAN);
+	window = SDL_CreateWindow("CHIP8 Emulator v1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, DISPLAY_WIDTH * SCALE, DISPLAY_HEIGHT * SCALE, SDL_WINDOW_VULKAN);
 	if (!window) {
 		std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
 		return false;
 	}
-	renderer = SDL_CreateRenderer(window, NULL);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	if (!renderer) {
 		std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
 		SDL_DestroyWindow(window);
@@ -33,7 +33,7 @@ void drawDisplay(SDL_Renderer* renderer, uint8_t* gfx) {
 		for (int x = 0; x < DISPLAY_WIDTH; ++x) {
 			if (gfx[y * DISPLAY_WIDTH + x]) {
 				SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-				SDL_FRect rect = { x * SCALE, y * SCALE, SCALE, SCALE };
+				SDL_Rect rect = { x * SCALE, y * SCALE, SCALE, SCALE };
 				SDL_RenderFillRect(renderer, &rect);
 			}
 		}
@@ -41,30 +41,30 @@ void drawDisplay(SDL_Renderer* renderer, uint8_t* gfx) {
 	SDL_RenderPresent(renderer);
 }
 
-void main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
 	std::cout << "CHIP8 Emulator v1" << std::endl;
 	CHIP8 chip8;
 	char* romPath = argv[1];
 	if (argv[1] == nullptr) {
 		std::cerr << "Usage: " << argv[0] << " <path_to_rom>" << std::endl;
-		return;
+		return 1;
 	}
 	chip8.loadRom(romPath);
 	SDL_Window* window = nullptr;
 	SDL_Renderer* renderer = nullptr;
 	if (!initSDL(window, renderer)) {
-		return;
+		return 2;
 	}
 	SDL_Event event;
 	bool running = true;
 	while (running) {
 		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_EVENT_QUIT) {
+			if (event.type == SDL_QUIT) {
 				running = false;
 			}
 		}
 		uint8_t keyState[16] = { 0 };
-		const bool* keys = SDL_GetKeyboardState(NULL);
+		const uint8_t* keys = SDL_GetKeyboardState(NULL);
 		keyState[0x1] = keys[SDL_SCANCODE_1];
 		keyState[0x2] = keys[SDL_SCANCODE_2];
 		keyState[0x3] = keys[SDL_SCANCODE_3];
@@ -82,8 +82,9 @@ void main(int argc, char *argv[]) {
 		keyState[0xB] = keys[SDL_SCANCODE_C];
 		keyState[0xF] = keys[SDL_SCANCODE_V];
 		chip8.setKeys(keyState);
-		
-		chip8.emulateCycle();
+		for (int i = 0; i < 10; ++i) { // Emulate 10 cycles for smoother timing
+			chip8.emulateCycle();
+		}
 		if(chip8.drawFlag) {
 			chip8.drawFlag = false;
 			drawDisplay(renderer, chip8.gfx);
@@ -94,4 +95,5 @@ void main(int argc, char *argv[]) {
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	std::cout << "Exiting..." << std::endl;
+	return 0;
 }
